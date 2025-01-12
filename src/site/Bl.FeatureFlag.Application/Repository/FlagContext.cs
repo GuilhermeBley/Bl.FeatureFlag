6e8ae24cf9a65bb0e4aa0f4c1857ea2c5d63b7ef
Guilherme Bley <guilhermecbley@gmail.com>
2025-01-12 19:21:32 -0300

    adding identity

--- src/site/Bl.FeatureFlag.Application/Repository/FlagContext.cs
+++ src/site/Bl.FeatureFlag.Application/Repository/FlagContext.cs
@@ -1,9 +1,11 @@
 ﻿using Bl.FeatureFlag.Application.Model;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
 namespace Bl.FeatureFlag.Application.Repository;
 
 public abstract class FlagContext
-    : DbContext
+    : IdentityDbContext<IdentityUser<Guid>, IdentityRole<Guid>, Guid>
 {
     public DbSet<CompleteFlagAccessModel> Flags { get; set; }
     public DbSet<FlagGroupModel> FlagGroups { get; set; }
