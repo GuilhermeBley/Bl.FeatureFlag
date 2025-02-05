d42092fd05843861ba1808c0375e60bff68ea17e
Guilherme Bley <guilhermecbley@gmail.com>
2025-02-04 23:40:27 -0300

    feat(GetAllFlagsFromSubscriptionResponse)

--- src/site/Bl.FeatureFlag.Application/Commands/GetAllFlagsFromSubscription/GetAllFlagsFromSubscriptionHandler.cs
+++ src/site/Bl.FeatureFlag.Application/Commands/GetAllFlagsFromSubscription/GetAllFlagsFromSubscriptionHandler.cs
@@ -1,6 +1,24 @@
-﻿namespace Bl.FeatureFlag.Application.Commands.GetAllFlagsFromSubscription;
+﻿
+using Bl.FeatureFlag.Application.Repository;
+
+namespace Bl.FeatureFlag.Application.Commands.GetAllFlagsFromSubscription;
 
 public class GetAllFlagsFromSubscriptionHandler
     : IRequestHandler<GetAllFlagsFromSubscriptionRequest, GetAllFlagsFromSubscriptionResponse>
 {
+    private readonly FlagContext _context;
+    private readonly IClaimProvider _claimProvider;
+
+    public async Task<GetAllFlagsFromSubscriptionResponse> Handle(
+        GetAllFlagsFromSubscriptionRequest request, 
+        CancellationToken cancellationToken)
+    {
+        cancellationToken.ThrowIfCancellationRequested();
+
+        var user = await _claimProvider.GetCurrentUserAsync(cancellationToken);
+
+        user.ThrowIfDoesntContainRole(Domain.Security.UserClaim.SeeFlags);
+
+
+    }
 }
--- src/site/Bl.FeatureFlag.Application/Commands/GetAllFlagsFromSubscription/GetAllFlagsFromSubscriptionResponse.cs
+++ src/site/Bl.FeatureFlag.Application/Commands/GetAllFlagsFromSubscription/GetAllFlagsFromSubscriptionResponse.cs
@@ -1,27 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace Bl.FeatureFlag.Application.Commands.GetAllFlagsFromSubscription;
 
-namespace Bl.FeatureFlag.Application.Commands.GetAllFlagsFromSubscription
+public record GetAllFlagsFromSubscriptionResponse(
+    GetAllFlagsFromSubscriptionSubInfo[] Infos);
+
+/// <summary>
+/// Subscription info with all flags
+/// </summary>
+public class GetAllFlagsFromSubscriptionSubInfo
 {
-    public class GetAllFlagsFromSubscriptionSubscriptionInfo
-    {
-        public Guid Id { get; set; }
-        public string Name { get; set; } = string.Empty;
-        public string NormalizedName { get; set; } = string.Empty;
-        public DateTime CreatedAt { get; set; }
-    }
+    public Guid Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string NormalizedName { get; set; } = string.Empty;
+    public GetAllFlagsFromSubscriptionFlagInfo[] Flags { get; set; } = [];
+    public DateTime CreatedAt { get; set; }
+}
 
 
-    public record GetAllFlagsFromSubscriptionResponse
-    {
-        public Guid FlagId { get; set; }
-        public string GroupName { get; set; } = string.Empty;
-        public string NormalizedGroupName { get; set; } = string.Empty;
-        public bool Active { get; set; }
-        public DateTime? ExpiresAt { get; set; }
-        public DateTime FlagCreatedAt { get; set; }
-    }
+public record GetAllFlagsFromSubscriptionFlagInfo
+{
+    public Guid FlagId { get; set; }
+    public string GroupName { get; set; } = string.Empty;
+    public string NormalizedGroupName { get; set; } = string.Empty;
+    public bool Active { get; set; }
+    public DateTime? ExpiresAt { get; set; }
+    public DateTime FlagCreatedAt { get; set; }
 }
