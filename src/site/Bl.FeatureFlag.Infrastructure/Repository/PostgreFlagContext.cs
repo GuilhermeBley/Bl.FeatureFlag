a77f43398be1c79688e140062ac5fa696fc64a9a
Guilherme Bley <guilhermecbley@gmail.com>
2025-02-12 00:31:19 -0300

    feat(PostgreFlagContext) - configuring CompleteFlagAccessModel

--- src/site/Bl.FeatureFlag.Infrastructure/Repository/PostgreFlagContext.cs
+++ src/site/Bl.FeatureFlag.Infrastructure/Repository/PostgreFlagContext.cs
@@ -1,6 +1,7 @@
 ﻿using Bl.FeatureFlag.Application.Model;
 using Bl.FeatureFlag.Application.Repository;
 using Microsoft.EntityFrameworkCore;
+using System;
 
 namespace Bl.FeatureFlag.Infrastructure.Repository;
 
@@ -15,7 +16,26 @@ internal class PostgreFlagContext
 
         builder.Entity<CompleteFlagAccessModel>(b =>
         {
+            b.HasKey(e => e.Id);
 
+            b.HasIndex(e => new { e.GroupId, e.NormalizedRoleName }).IsUnique();
+
+            b.Property(e => e.Active).IsRequired();
+            b.Property(e => e.Description).IsRequired();
+
+            b.Property(e => e.GroupId).IsRequired();
+            b.Property(e => e.RoleName).IsRequired().HasMaxLength(255);
+            b.Property(e => e.NormalizedRoleName).IsRequired().HasMaxLength(255);
+            b.Property(e => e.Active).IsRequired();
+            b.Property(e => e.ExpiresAt);
+            b.Property(e => e.CreatedAt).IsRequired();
+            b.Property(e => e.Description);
+            b.Property(e => e.Obs);
+
+            b.HasOne<FlagGroupModel>()
+                .WithMany()
+                .HasForeignKey(e => e.GroupId)
+                .OnDelete(DeleteBehavior.NoAction);
         });
 
         builder.Entity<FlagGroupModel>(b =>
