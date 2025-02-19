7be58016836ed73e55853043abb242ea39d82d6f
Guilherme Bley <guilhermecbley@gmail.com>
2025-02-18 23:25:24 -0300

    feat(FlagEndpoints) - adding first GET

---
+++ src/site/Bl.FeatureFlag.Api/Endpoints/FlagEndpoints.cs
@@ -0,0 +1,26 @@
+﻿using Bl.FeatureFlag.Application.Providers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bl.FeatureFlag.Api.Endpoints;
+
+public static class FlagEndpoints
+{
+    public static void MapEndpoints(
+        IEndpointRouteBuilder endpointBuilder)
+    {
+        endpointBuilder.MapGet(
+            "api/flag/subscription/{subscriptionId}",
+            async (
+                Guid subscriptionId,
+                HttpContext context,
+                [FromServices] IMediator mediator) =>
+            {
+                var result = await mediator.Send(
+                    new Application.Commands.GetAllFlagsFromSubscription.GetAllFlagsFromSubscriptionRequest(
+                        SubscriptionId: subscriptionId, 
+                        UserId: context.User.RequiredUserId()));
+
+                return Results.Ok();
+            });
+    }
+}
---
+++ src/site/Bl.FeatureFlag.Api/GlobalUsings.cs
@@ -0,0 +1,2 @@
+﻿global using MediatR;
+global using Bl.FeatureFlag.Application.Extensions;
